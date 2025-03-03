e0d9e57febeeb9c4b0df14aa44845c5294b18378
radhoda <rod.hoda@gmail.com>
2025-03-02 20:29:10 -0800

    Added a few more route handlers for the different pages we will have

--- app.py
+++ app.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "To-Do Clock"
+	return "Welcome to To-Do Clock!"
+
+@app.route('/clock-view')
+def clock_view():
+	return "Clock view + button to add task"
+
+@app.route('/task-list')
+def task_list_view():
+	return "List of tasks + button to add task"
+
+@app.route('/add-task')
+def add_task():
+	return "Adding a task and all features to go along with it"
 
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0', port=3000)
